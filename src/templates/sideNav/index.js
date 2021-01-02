9b96df2bc6f0f16ab0e312086418bd0387c67549
Guilherme de Almeida Costa <costaguila@gmail.com>
2021-01-02 19:39:50 -0300

    Adding color scheme

--- src/templates/sideNav/index.js
+++ src/templates/sideNav/index.js
@@ -4,11 +4,15 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 function SideNav(props) {
   return (
-    <Container fluid style={{ height: '100vh'}}>
+    <Container fluid style={{ height: '100vh' }} >
     <Row style={{ height: '100vh'}}>
-      <Col sm={4} md={2}  style={{backgroundColor: 'black'}}>
+      <Col sm={4} md={2} className="sidenav">
+        <p className="nav-option">Home</p>
+        <p className="nav-option">vechicles</p>
+        <p className="nav-option">Map</p>
+        <p className="nav-option">About</p>
       </Col>
-      <Col sm={8} md={10} style={{backgroundColor: 'red'}}>
+      <Col sm={8} md={10} className="main-container">
         {props.children}
       </Col>
     </Row>
